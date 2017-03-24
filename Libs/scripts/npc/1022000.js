b5d2e2236513e108d4e8036cb4d8b19451254747
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-24 19:53:34 +0800

    修改指令位子

--- Libs/scripts/npc/1022000.js
+++ Libs/scripts/npc/1022000.js
@@ -50,9 +50,9 @@ function action(mode, type, selection) {
 		    status = 10;
 		    cm.sendNext("看來你越來越強了!");
 		}
-	    } else if (cm.getQuestStatus(100100) == 1 ) {
+	    } else if (cm.getQuestStatus(100100) == 1  && cm.haveItem(4031059)) {
 		cm.completeQuest(100101);
-		if (cm.getQuestStatus(100101) == 2 && cm.haveItem(4031059)) {
+		if (cm.getQuestStatus(100101) == 2) {
 		    cm.sendOk("看來你已經證明了你自己，把這個交給#b泰勒斯#k。");
 		} else {
 		    cm.sendOk("#b#h0##k! 如果你要獲得我的認同，就得去異界打倒我的分身取得#b黑符#k，證明你自己。我將會把你傳送到通往古老路上的次元裂縫，它將可以帶你到異界。");
