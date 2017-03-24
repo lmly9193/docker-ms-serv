7c08ffa7c644cd09dbdee5f031f53a505f37ee4e
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-24 23:59:49 +0800

    修改NPC 內容

--- Libs/scripts/npc/1072000.js
+++ Libs/scripts/npc/1072000.js
@@ -1,56 +1,44 @@
-/*
-	This file is part of the OdinMS Maple Story Server
-    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc> 
-                       Matthias Butz <matze@odinms.de>
-                       Jan Christian Meyer <vimes@odinms.de>
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License version 3
-    as published by the Free Software Foundation. You may not use, modify
-    or distribute this program under any other version of the
-    GNU Affero General Public License.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/* Warrior Job Instructor
+	Warrior 2nd Job Advancement
+	Victoria Road : West Rocky Mountain IV (102020300)
 */
 
-/* Magician Job Instructor
-*/
-
-var status;
-
-function start() {
-	status = -1;
-	action(1, 0, 0);
-}
+var status = -1;
 
 function action(mode, type, selection) {
-    if (mode == -1)
-        cm.dispose();
-    else {
-        if (mode == 1)
-            status++;
-        else
-            status--;
-
-			if (cm.haveItem(4031008)) {
-				if (status == 0)
-					cm.sendNext("噢, 你是 #b武術教練#k 介紹來的嗎")
-				else if (status == 1)
-					cm.sendNextPrev("所以你要證明你的實力嗎 ? 很好...");
-				else if (status == 2)
-					cm.sendNextPrev("我可以給你一次機會,請你把握.");
-				else if (status == 3)
-					cm.sendYesNo("請給我 #b30 #t4031013##k. 祝你好運.");
-				else if (status == 4) {
-					cm.warp(108000300, 0);
-					cm.dispose();
-				}
-			} else {
-				cm.sendOk("很抱歉,我需要 #b武術教練的信件#k 請去找武術教練拿取謝謝");
-				cm.dispose();
-			}
+    if (mode == 1) {
+	status++;
+    } else {
+	status--;
+    }
+    if (status == 0) {
+	if (cm.getQuestStatus(100004) == 1) {
+	    cm.sendOk("You will have to collect me #b30 #t4031013##k. Good luck.");
+	    status = 3;
+	} else {
+	    if (cm.getQuestStatus(100004) == 2) {
+		cm.sendOk("You're truly a hero!");
+		cm.safeDispose();
+	    } else if (cm.getQuestStatus(100003) >= 1) {
+		cm.completeQuest(100003);
+		if (cm.getQuestStatus(100003) == 2) {
+		    cm.sendNext("Oh, isn't this a letter from #bDances with Balrog#k?");
+		}
+	    } else {
+		cm.sendOk("I can show you the way once your ready for it.");
+		cm.safeDispose();
+	    }
+	}
+    } else if (status == 1) {
+	cm.sendNextPrev("So you want to prove your skills? Very well...")
+    } else if (status == 2) {
+	cm.askAcceptDecline("I will give you a chance if you're ready.");
+    } else if (status == 3) {
+	cm.startQuest(100004);
+	cm.sendOk("You will have to collect me #b30 #t4031013##k. Good luck.")
+    } else if (status == 4) {
+	//	    cm.gainItem(4031008, -1);
+	cm.warp(108000300, 0);
+	cm.dispose();
     }
 }
