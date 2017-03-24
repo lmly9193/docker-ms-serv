356b45ddc77de52b70a7c6cb57110099f9d0e1bc
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-25 00:55:22 +0800

    神聖石NPC 修改

--- Libs/scripts/npc/2030006.js
+++ Libs/scripts/npc/2030006.js
@@ -1,3 +1,4 @@
+
 /* Holy Stone
 	Hidden Street: Holy Ground at the Snowfield (211040401)
 	
@@ -22,21 +23,22 @@ function action(mode, type, selection) {
 	status--;
     if (status == 0) {
 	if (cm.haveItem(4031058) || cm.getQuestStatus(100102) != 1) {
-	    cm.sendOk("Lulz, I am a stone.");
+	    cm.sendOk("我是一個石頭.");
 	    cm.dispose();
 	} else {
-	    cm.sendNext("Lulz, I am a stone.");
+	    cm.sendNext("我是一個石頭.");
 	}
     } else if (status == 1) {
-	cm.sendNextPrev("Give me a #bDark Crystal#k and I will allow you to obtain the #bNecklace of Wisdom#k.");
+	cm.sendNextPrev("給我#b黑暗水晶#k 且回答我的問題，我會給你#b智慧項鍊#k.");
     } else if (status == 2) {
 	if (!cm.haveItem(4005004)) {
-	    cm.sendOk("You don't have any #bDark Crystal#ks.");
+	    cm.sendOk("你沒有#b黑暗水晶#k.");
 	    cm.dispose();
 	} else {
+		cm.completeQuest(100102);
 	    cm.gainItem(4005004, -1);
 	    cm.gainItem(4031058, 1);
-	    cm.sendOk("Indeed.");
+	    cm.sendOk("完成.");
 	    cm.dispose();
 	}
     }
