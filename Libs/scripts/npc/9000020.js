148f9a620947c87e8c19b7c7eeaa7fb8ff8fcfa7
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-28 02:07:07 +0800

    NPC導遊 修改

--- Libs/scripts/npc/9000020.js
+++ Libs/scripts/npc/9000020.js
@@ -1,93 +1,229 @@
+/*
+	NPC Name: 		Spinel
+	Map(s): 		Victoria Road : Henesys (100000000), Victoria Road : Ellinia (101000000), Victoria Road : Perion (102000000), Victoria Road : Kerning City (103000000), Victoria Road : Lith Harbor (104000000), Orbis : Orbis (200000000), Ludibrium : Ludibrium (220000000), Leafre : Leafre (240000000), Zipangu : Mushroom Shrine (800000000)
+	Description: 		World Tour Guide
+*/
 var status = -1;
 var cost, sel;
-var togo1, togo2, togo3;
+var togo1, togo2, togo3, togo4, togo5, togo6, togo7, togo8, togo9, togo10;
 var map;
-var back = true;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 function start() {
-    switch (cm.getMapId()) {
-	case 800000000:
-	case 500000000:
-	case 701000000:
-	case 740000000:
-	    map = cm.getSavedLocation("WORLDTOUR");
-	    cm.sendSimple("在這裡玩的如何呢?想去別的地方繼續旅遊還是回到原來的地方呢? \n\r #b#L0#我還可以去哪邊?#l \n\r #L1#我旅行完了,我要回去#m"+map+"##l");
-	    break;
-	default:
-	    back = false;
-	    if (cm.getJob() == 0) {
-		cm.sendNext("HI 我是旅遊小幫手，可以帶妳去各地遊玩\n\r 咦!原來你是初心者呀 這樣的話我也不收你太多\r\n#b300楓幣就好了");
-		cost = 300;
-	    } else {
-		cm.sendNext("HI 我是旅遊小幫手，可以帶妳去各地遊玩，收你3000楓幣就好了");
-		cost = 3000;
-	    }
-	    break;
-    }
+	switch (cm.getMapId()) {
+		case 200000000: //天空之城1
+		case 211000000: //冰原雪域2
+		case 250000000: //桃花仙境3
+		case 260000000: //沙漠4
+		case 220000000: //玩具城5
+		case 240000000:	//神木村6
+		case 140000000: //瑞恩7
+		case 740000000: //西門町8
+		case 701000000: //上海灘9
+		case 500000000: //泰國10
+		case 800000000: //日本神社11
+			map = cm.getSavedLoccation("Worldtour");
+			cm.sendSimple("想到處旅遊嗎?找我就對了，哈哈!!想去哪呢?還是要回去原來的地方? \n\r #b#L0#我想去~#l \n\r #L1#我旅行完了,我要回去#m"+map+"##l");
+			break;
+	}
 }
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function action(mode, type, selection) {
-    if (mode == -1) {
-	cm.dispose();
-    } else {
-	if ((status <= 2 && mode == 0) || (status == 4 && mode == 1)) {
-	    cm.dispose();
-	    return;
+	if (mode == -1) { 
+		cm.dispose();
+	}else {
+		if((status <= 2 && mode == 0)) || (status == 4 && mode == 1)) {
+			cm.dispose;
+			return;
+		}
 	}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	if (mode == 1)
-	    status++;
+		status++;
 	else
-	    status--;
-    
-	    if (status == 0) {
+		status--;
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	if (status == 0) {
 		if (selection == 0) {
-		    switch (cm.getMapId()) {
-			case 740000000:
-			    togo1 = 800000000;
-			    togo2 = 701000000;
-			    togo3 = 500000000;
-			case 500000000:
-			    togo1 = 800000000;
-			    togo2 = 701000000;
-			    togo3 = 740000000;
-			    break;
-			case 800000000:
-			    togo1 = 701000000;
-			    togo2 = 500000000;
-			    togo3 = 740000000;
-			    break;
-			case 701000000:
-			    togo1 = 500000000;
-			    togo2 = 800000000;
-			    togo3 = 740000000;
-			    break;
+			switch (cm.getMapId()) {
+				case 200000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 211000000:
+					togo1 = 200000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 250000000:
+					togo1 = 211000000;
+					togo2 = 200000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 260000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 200000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 220000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 200000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 240000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 200000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 140000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 200000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 740000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 200000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 701000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 200000000;
+					togo9 = 500000000;
+					togo10 = 800000000;
+				case 500000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 200000000;
+					togo10 = 800000000;
+				case 800000000:
+					togo1 = 211000000;
+					togo2 = 250000000;
+					togo3 = 260000000;
+					togo4 = 220000000;
+					togo5 = 240000000;
+					togo6 = 140000000;
+					togo7 = 740000000;
+					togo8 = 701000000;
+					togo9 = 500000000;
+					togo10 = 200000000;
+				break;
+			}
+			cm.sendSimple("想去哪呢? \n\r #b#L0##m"+togo1+"# (3,000 楓幣)#l \n\r #b#L1##m"+togo2+"# (3,000 楓幣)#l \n\r #b#L2##m"+togo3+"# (3,000 楓幣)#l \n\r #b#L3##m"+togo4+"# (3,000 楓幣)#l \n\r #b#L4##m"+togo5+"# (3,000 楓幣)#l \n\r #b#L5##m"+togo6+"# (3,000 楓幣)#l \n\r #b#L6##m"+togo7+"# (3,000 楓幣)#l \n\r #b#L7##m"+togo8+"# (3,000 楓幣)#l \n\r #b#L8##m"+togo9+"# (3,000 楓幣)#l \n\r #b#L9##m"+togo10+"# (3,000 楓幣)#l");
+			}else if (selection == 1){
+				cm.warp(map);
+				cm.clearSavedLocation("Worldtour");
+				cm.dispose();
+			}
+		}else if (status == 1) {
+			sel = selection;
+			if (sel == 0 ) {
+				cm.sendNext("你確定要前往 #b#m"+togo1+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 1 ){
+				cm.sendNext("你確定要前往 #b#m"+togo2+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 2 ){
+				cm.sendNext("你確定要前往 #b#m"+togo3+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 3 ){
+				cm.sendNext("你確定要前往 #b#m"+togo4+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 4 ){
+				cm.sendNext("你確定要前往 #b#m"+togo5+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 5 ){
+				cm.sendNext("你確定要前往 #b#m"+togo6+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 6 ){
+				cm.sendNext("你確定要前往 #b#m"+togo7+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 7 ){
+				cm.sendNext("你確定要前往 #b#m"+togo8+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 8 ){
+				cm.sendNext("你確定要前往 #b#m"+togo9+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}else if (sel == 9 ){
+				cm.sendNext("你確定要前往 #b#m"+togo10+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
+			}
+		}else if (status == 2) {
+			if (sel == 0 ) {
+				cm.warp(togo1);
+			}else if (sel == 1) {
+		    	cm.warp(togo2);
+		    }else if (sel == 2) {
+		    	cm.warp(togo3);
+		    }else if (sel == 3) {
+		    	cm.warp(togo4);
+		    }else if (sel == 4) {
+		    	cm.warp(togo5);
+		    }else if (sel == 5) {
+		    	cm.warp(togo6);
+		    }else if (sel == 6) {
+		    	cm.warp(togo7);
+		    }else if (sel == 7) {
+		    	cm.warp(togo8);
+		    }else if (sel == 8) {
+		    	cm.warp(togo9);
+		    }else if (sel == 9) {
+		    	cm.warp(togo10);
 		    }
-		    cm.sendSimple("想去哪裡呢? \n\r #b#L0##m"+togo1+"# (3,000 楓幣)#l \n\r #L1##m"+togo2+"# (3,000 楓幣)#l \n\r #L2##m"+togo3+"# (3,000 楓幣)#l");
-
-		} else if (selection == 1) {
-		    cm.warp(map == -1 ? 100000000 : map);
-		    cm.clearSavedLocation("WORLDTOUR");
 		    cm.dispose();
 		}
-	    } else if (status == 1) {
-		sel = selection;
-		if (sel == 0) {
-		    cm.sendNext("你確定要前往 #b#m"+togo1+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
-		} else if (sel == 1) {
-		    cm.sendNext("你確定要前往  #b#m"+togo2+"##k? 到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
-		} else if (sel == 2) {
-		    cm.sendNext("你確定要前往  #b#m"+togo3+"##k? 到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
-		}
-	    } else if (status == 2) {
-		if (sel == 0) {
-		    cm.warp(togo1);
-		} else if (sel == 1) {
-		    cm.warp(togo2);
-		} else if (sel == 2) {
-		    cm.warp(togo3);
-		}
-		cm.dispose();
-	    }
 	}
-    }
 }
+				
