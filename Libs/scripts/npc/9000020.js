e2de65eaa48076c9929bd22354c2d55191e4f312
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-28 03:45:54 +0800

    NPC 導遊 新增 傳送地圖

--- Libs/scripts/npc/9000020.js
+++ Libs/scripts/npc/9000020.js
@@ -20,7 +20,7 @@ function action (mode, type, selection) {
 	}
 	else if (status == 1){
 		if (selection == 0){
-			cm.sendSimple("我可以帶你去~ \n\r #b#L0##m800000000##l \n\r #L1##m200000000##l");
+			cm.sendSimple("我可以帶你去~ \n\r #b#L0##m200000000##l \n\r #L1##m211000000##l \n\r #b#L2##m250000000##l \n\r #b#L3##m260000000##l \n\r #b#L4##m220000000##l \n\r #b#L5##m240000000##l \n\r #b#L6##m140000000##l \n\r #b#L7##m740000000##l \n\r #b#L8##m700000000##l \n\r #b#L9##m500000000##l \n\r #b#L10##m800000000##l");
 		}else if (selection == 1){
 			cm.sendOk("那有想旅遊可以再找我:)");
 			cm.dispose();
@@ -28,9 +28,27 @@ function action (mode, type, selection) {
 	}
 	else if (status == 2){
 		if (selection == 0){
-			cm.warp(800000000,0)
-		}else if (selection == 1){
 			cm.warp(200000000,0)
+		}else if (selection == 1){
+			cm.warp(211000000,0)
+		}else if (selection == 2){
+			cm.warp(250000000,0)
+		}else if (selection == 3){
+			cm.warp(260000000,0)
+		}else if (selection == 4){
+			cm.warp(220000000,0)
+		}else if (selection == 5){
+			cm.warp(240000000,0)
+		}else if (selection == 6){
+			cm.warp(140000000,0)
+		}else if (selection == 7){
+			cm.warp(740000000,0)
+		}else if (selection == 8){
+			cm.warp(700000000,0)
+		}else if (selection == 9){
+			cm.warp(750000000,0)
+		}else if (selection == 10){
+			cm.warp(800000000,0)
 		}
 		cm.dispose();
 	}
