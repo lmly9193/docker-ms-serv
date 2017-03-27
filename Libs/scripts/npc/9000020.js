5923512730e55baa89868b8a43baafe3ef463209
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-27 21:41:54 +0800

    導遊 修改回原版

--- Libs/scripts/npc/9000020.js
+++ Libs/scripts/npc/9000020.js
@@ -17,19 +17,15 @@ function start() {
 	case 701000000:
 	case 740000000:
 	    map = cm.getSavedLocation("WORLDTOUR");
-	    cm.sendSimple("How's the traveling? Are you enjoying it? \n\r #b#L0# Can I go somewhere else?#l \n\r #L1# I'm done with traveling. Can I go back to #m"+map+"#?#l");
-	    break;
-	case 950000100:
-	    map = 193000000;
-	    cm.sendSimple("How's the traveling? Are you enjoying it? \n\r #b#L1# I'm done with traveling. Can I go back to #m"+map+"#?#l");
+	    cm.sendSimple("在這裡玩的如何呢?想去別的地方繼續旅遊還是回到原來的地方呢? \n\r #b#L0#我還可以去哪邊?#l \n\r #L1#我旅行完了,我要回去#m"+map+"##l");
 	    break;
 	default:
 	    back = false;
 	    if (cm.getJob() == 0) {
-		cm.sendNext("If you're tired of the monotonous daily life, how about getting out for a change? There's nothing quite like soaking up a new culture, learning something new by the minute! It's time for you to get out and travel. We recommend a\r\n#bWorld Tour#k! Are you worried about the travel expense? No need to worry! The #bMaple Travel Agency#k offers first class travel accommodation for the low price of #b300 mesos#k.");
+		cm.sendNext("HI 我是旅遊小幫手，可以帶妳去各地遊玩\n\r 咦!原來你是初心者呀 這樣的話我也不收你太多\r\n#b300楓幣就好了");
 		cost = 300;
 	    } else {
-		cm.sendNext("If you're tired of the monotonous daily life, how about getting out for a change? There's nothing quite like soaking up a new culture, learning something new by the minute! It's time for you to get out and travel. We, at the Maple Travel Agency recommend you going on a #bWorld Tour#k! Are you worried about the travel expense? You shouldn't be! We, the #bMaple Travel Agency#k, have carefully come up with a plan to let you travel for ONLY #b3,000 mesos!#k");
+		cm.sendNext("HI 我是旅遊小幫手，可以帶妳去各地遊玩，收你3000楓幣就好了");
 		cost = 3000;
 	    }
 	    break;
@@ -51,14 +47,14 @@ function action(mode, type, selection) {
 
 	if (!back) {
 	    if (status == 0) {
-		cm.sendSimple("We currently offer this place for your traveling pleasure:\r\n#bMushroom Shrine of Japan#k. I'll be there serving you as the travel guide. Rest assured, the number of destinations will increase over time. Now, would you like to head over to the Mushroom Shrine?\r\n#L0##b Yes, take me to Mushroom Shrine (Japan)#k#l");
+		cm.sendSimple("我們目前只有提供到日本旅遊\r\n#L0##b#m800000000");
 	    } else if (status == 1) {
-		cm.sendYesNo("Would you like to travel to the #bMushroom Shrine of Japan#k?\r\nIf you desire to feel the essence of Japan, there's nothing like visiting the Shrine, a Japanese cultural melting pot. Mushroom Shrine is a mythical place that serves the incomparable Mushroom God from ancient times.");
+		cm.sendYesNo("日本古代神社充滿者有趣的氣息，請問你是否要前往?");
+	    /*} else if (status == 2) {
+		cm.sendNext("Check out the female shaman serving the Mushroom God, and I strongly recommend trying Takoyaki, Yakisoba, and other delicious food sold in the streets of Japan. Now, let's head over to #bMushroom Shrine#k, a mythical place if there ever was one.");*/
 	    } else if (status == 2) {
-		cm.sendNext("Check out the female shaman serving the Mushroom God, and I strongly recommend trying Takoyaki, Yakisoba, and other delicious food sold in the streets of Japan. Now, let's head over to #bMushroom Shrine#k, a mythical place if there ever was one.");
-	    } else if (status == 3) {
 		if (cm.getMeso() < cost) {
-		    cm.sendPrev("Please check and see if you have enough mesos to go.");
+		    cm.sendPrev("不好意思你的金錢不夠喔");
 		} else {
 		    cm.gainMeso(-cost);
 		    cm.saveLocation("WORLDTOUR");
@@ -89,11 +85,8 @@ function action(mode, type, selection) {
 			    togo2 = 800000000;
 			    togo3 = 740000000;
 			    break;
-			default:
-			    cm.dispose();
-			    return;
 		    }
-		    cm.sendSimple("Where would you like to travel? \n\r #b#L0##m"+togo1+"# (3,000 mesos)#l \n\r #L1##m"+togo2+"# (3,000 mesos)#l \n\r #L2##m"+togo3+"# (3,000 mesos)#l");
+		    cm.sendSimple("想去哪裡呢? \n\r #b#L0##m"+togo1+"# (3,000 楓幣)#l \n\r #L1##m"+togo2+"# (3,000 楓幣)#l \n\r #L2##m"+togo3+"# (3,000 楓幣)#l");
 
 		} else if (selection == 1) {
 		    cm.warp(map == -1 ? 100000000 : map);
@@ -103,11 +96,11 @@ function action(mode, type, selection) {
 	    } else if (status == 1) {
 		sel = selection;
 		if (sel == 0) {
-		    cm.sendNext("Would you like to travel to #b#m"+togo1+"##k? To head over, it'll cost you only #b3,000 mesos#k. Would you like to go right now?");
+		    cm.sendNext("你確定要前往 #b#m"+togo1+"##k?  到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
 		} else if (sel == 1) {
-		    cm.sendNext("Would you like to travel to #b#m"+togo2+"##k? To head over, it'll cost you only #b3,000 mesos#k. Would you like to go right now?");
+		    cm.sendNext("你確定要前往  #b#m"+togo2+"##k? 到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
 		} else if (sel == 2) {
-		    cm.sendNext("Would you like to travel to #b#m"+togo3+"##k? To head over, it'll cost you only #b3,000 mesos#k. Would you like to go right now?");
+		    cm.sendNext("你確定要前往  #b#m"+togo3+"##k? 到那邊需要 #b3,000 楓幣#k. 確定現在要去嗎?");
 		}
 	    } else if (status == 2) {
 		if (sel == 0) {
