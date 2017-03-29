cec0246b96fe64a13111d7dbf0c0904408f09d51
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-30 06:25:15 +0800

    炎魔前置

--- Libs/scripts/npc/2030008.js
+++ Libs/scripts/npc/2030008.js
@@ -3,15 +3,18 @@
     Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc> 
                        Matthias Butz <matze@odinms.de>
                        Jan Christian Meyer <vimes@odinms.de>
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3
     as published by the Free Software Foundation. You may not use, modify
     or distribute this program under any other version of the
     GNU Affero General Public License.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
+
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -167,7 +170,6 @@ function action(mode, type, selection) {
                         cm.sendNext("Thank you for the teeth!  Next time you see me, I'll be blinging harder than #rJaws#k!  Goodbye and good luck!");
                         cm.completeQuest(100201);
                         cm.completeQuest(100200);
-                        cm.completeQuest(100203);
                         cm.dispose();
                     }
                     else {
