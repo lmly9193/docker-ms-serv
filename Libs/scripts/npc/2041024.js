06e83beec63f1afbd6c57b445926d80fda5a0c87
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-30 12:33:35 +0800

    NPC 修改

--- Libs/scripts/npc/2041024.js
+++ Libs/scripts/npc/2041024.js
@@ -4,14 +4,14 @@ function start(){
 	status = -1;
 	action(1, 0, 0); 
 }
-function action (mode, type, selection) {
+function action (mode,type,selection) {
 	if (mode == 1){
 		status++;
 	}else{
 		status--;
 	}
 	if (status == 0){
-		cm.sendYesNo("你想要時空碎片D嗎?");
+		cm.sendNext("你想要時空碎片D嗎?");
 	}else if (status == 1 ) {
 		if(cm.haveitem(4031172)){
 			cm.sendOk("拿去好好利用吧，需要再跟我拿~ 記得一天只能打兩次!!");
