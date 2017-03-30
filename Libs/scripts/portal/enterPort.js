f8d2ef8f1843b9529f6671c30b9dd50204d4d264
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 07:09:39 +0800

    修復狂狼勇士 3轉 到港口沒有 怪打

--- Libs/scripts/portal/enterPort.js
+++ Libs/scripts/portal/enterPort.js
@@ -1,3 +1,10 @@
+var map = 108010700;
+
 function enter(pi) {
-    pi.warp(140020300, 1);
+	if (pi.getQuestStatus(21301) == 1) {
+		pi.resetMap(map);
+		pi.warp(map, 1);
+	} else {
+		pi.warp(140020300, 1);
+	}
 }
