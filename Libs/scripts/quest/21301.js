759c33e627eef91af69bc4fb548d2d30833cd5a1
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 10:12:12 +0800

    修改內容

--- Libs/scripts/quest/21301.js
+++ Libs/scripts/quest/21301.js
@@ -2,13 +2,13 @@ var status = -1;
 
 function start(mode, type, selection) {
 	qm.forceStartQuest(21301);
+	qm.completeQuest(21301);
 	qm.dispose();
 }
 
 function end(mode, type, selection) {
 	if (qm.getQuestStatus(21301) == 0) {
 		qm.forceStartQuest(21301);
-	} else if (qm.getQuestStatus(21301) == 1) {
 		qm.forceCompleteQuest();
 	}
 	qm.dispose();
