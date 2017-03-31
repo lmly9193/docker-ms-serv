936c6e7a9792266784c53ee03f65e14044377808
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 09:51:47 +0800

    還原初始檔案

--- Libs/scripts/quest/21302.js
+++ Libs/scripts/quest/21302.js
@@ -1,4 +1,3 @@
-
 var status = -1;
 
 function start(mode, type, selection) {
@@ -7,13 +6,6 @@ function start(mode, type, selection) {
 }
 
 function end(mode, type, selection) {
-	if (qm.getQuestStatus(21302) == 0) {
-		qm.forceStartQuest(21302);
-	} else if (qm.getQuestStatus(21302) == 1) {
-		qm.gainItem(1142131, 1)
-		qm.changeJob(2111);
-		qm.getPlayer().gainAp(5);
-		qm.forceCompleteQuest();
-	}
+	qm.forceStartQuest(21302);
 	qm.dispose();
 }
