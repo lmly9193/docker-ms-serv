d59b71ef381d6874e75650a2679e864d9a5eb166
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 10:30:07 +0800

    修改 NPC 轉職任務內容

--- Libs/scripts/quest/21301.js
+++ Libs/scripts/quest/21301.js
@@ -2,13 +2,16 @@ var status = -1;
 
 function start(mode, type, selection) {
 	qm.forceStartQuest(21301);
-	qm.completeQuest(21301);
 	qm.dispose();
 }
 
 function end(mode, type, selection) {
 	if (qm.getQuestStatus(21301) == 0) {
 		qm.forceStartQuest(21301);
+	} else if (qm.getQuestStatus(21301) == 1) {
+		qm.gainItem(1142131, 1)
+		qm.changeJob(2111);
+		qm.getPlayer().gainAp(5);
 		qm.forceCompleteQuest();
 	}
 	qm.dispose();
