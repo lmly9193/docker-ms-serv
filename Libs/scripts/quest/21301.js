ca1de5427c025ec5232a0e977d2fc7acaa89f7ae
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 10:39:12 +0800

    修復回初始檔案

--- Libs/scripts/quest/21301.js
+++ Libs/scripts/quest/21301.js
@@ -6,13 +6,6 @@ function start(mode, type, selection) {
 }
 
 function end(mode, type, selection) {
-	if (qm.getQuestStatus(21301) == 0) {
-		qm.forceStartQuest(21301);
-	} else if (qm.getQuestStatus(21301) == 1) {
-		qm.gainItem(1142131, 1)
-		qm.changeJob(2111);
-		qm.getPlayer().gainAp(5);
-		qm.forceCompleteQuest();
-	}
+	qm.forceStartQuest(21301);
 	qm.dispose();
 }
