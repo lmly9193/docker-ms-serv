8ea11547c6b6a3791040db414f73ce1ee76853d6
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 09:43:01 +0800

    修改指令錯誤

--- Libs/scripts/quest/21301.js
+++ Libs/scripts/quest/21301.js
@@ -1,11 +1,11 @@
 var status = -1;
 
 function start(mode, type, selection) {
-	qm.StartQuest(21301);
+	qm.startQuest(21301);
 	qm.dispose();
 }
 
 function end(mode, type, selection) {
-	qm.StartQuest(21301);
+	qm.startQuest(21301);
 	qm.dispose();
 }
