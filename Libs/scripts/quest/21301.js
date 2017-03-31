fe5fcab7eb90a8253ea8edfb6f5784a0614268ed
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 08:17:50 +0800

    修改NPC 任務 狂狼勇士 3轉

--- Libs/scripts/quest/21301.js
+++ Libs/scripts/quest/21301.js
@@ -6,10 +6,6 @@ function start(mode, type, selection) {
 }
 
 function end(mode, type, selection) {
-	if (qm.getQuestStatus(21301) == 0) {
-		qm.forceStartQuest(21301);
-	} else if (qm.getQuestStatus(21301) == 1) {
-		qm.forceCompleteQuest();
-	}
+	qm.forceStartQuest(21301);
 	qm.dispose();
 }
