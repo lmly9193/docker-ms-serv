8e3bbf16fc9ab82ebb0d676093f6d86bef3aa595
Michaelyu0120 <michaelyu0120@gmail.com>
2017-03-31 11:07:02 +0800

    修改傳送點

--- Libs/scripts/portal/enterMCave.js
+++ Libs/scripts/portal/enterMCave.js
@@ -1,11 +1,13 @@
+
 function enter(pi) {
-    if (pi.isQuestActive(21201)) { //aran first job
-	pi.forceCompleteQuest(21201);
-	pi.playerMessage(5, "You recovered your memories!");
-    }
-    pi.warp(108000700,0);
-	}else if (pi.getQuestStatus(21302) == 0)
-		pi.warp(108010701,0);
+	if (pi.isQuestActive(21201)) { //aran first job
+		pi.forceCompleteQuest(21201);
+		pi.playerMessage(5, "You recovered your memories!");
 	}
- //what does this even do
+	if (pi.getQuestStatus(21302) == 1) {
+		pi.resetMap(108010702);
+		pi.warp(108010701, 0);
+	} else {
+		pi.warp(108000700, 0);
+	} //what does this even do
 }
