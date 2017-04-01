5c295a6bad77e738730dc385c324602850c34338
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-02 02:01:59 +0800

    狂狼勇士轉職道具

--- Libs/scripts/quest/21303.js
+++ Libs/scripts/quest/21303.js
@@ -7,10 +7,9 @@ function start(mode, type, selection) {
 function end (mode, type, selection) {
 	if (qm.getQuestStatus(21303) == 0) {
 		qm.forceStartQuest(21303);
-		qm.gainItem(4032312,1);
 	}else if (qm.getQuestStatus(21301) == 1) {
 		qm.forceCompleteQuest();
-		qm.gainItem(4032312,1);
 	}
+	qm.gainItem(4032312,1);
 	qm.dispose();
 }
