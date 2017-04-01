11b0bfd8e54f69577965db9f0ea211cdb0f2d106
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-02 02:00:09 +0800

    狂狼轉職 任務道具

--- Libs/scripts/quest/21303.js
+++ Libs/scripts/quest/21303.js
@@ -7,6 +7,7 @@ function start(mode, type, selection) {
 function end (mode, type, selection) {
 	if (qm.getQuestStatus(21303) == 0) {
 		qm.forceStartQuest(21303);
+		qm.gainItem(4032312,1);
 	}else if (qm.getQuestStatus(21301) == 1) {
 		qm.forceCompleteQuest();
 		qm.gainItem(4032312,1);
