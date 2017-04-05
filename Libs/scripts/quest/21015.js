ec6613c7fda79ada665c51f5eb49e3796e303640
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-05 22:11:20 +0800

    修正翻譯內容

--- Libs/scripts/quest/21015.js
+++ Libs/scripts/quest/21015.js
@@ -16,7 +16,7 @@ function start(mode, type, selection) {
     } else if (status == 1) {
 	qm.sendNextPrev("過去你可能是一個英雄，但那是數百年前的事。即使不是黑魔法師的詛咒，那些凍結的時間，都會使你的身體僵硬和能力下降。你必須訓練，慢慢恢復你的能力。你說?要怎麼做?");
     } else if (status == 2) {
-	qm.askAcceptDecline("你知道必須先掌握基礎知識嗎？所以明智的做法是從#b基礎訓練＃k開始。哦，我知道忘記所有事了。那麼這就是為什麼我出現在這裡。 你必需要自己體驗一下。那我們開始吧？");
+	qm.askAcceptDecline("你知道必須先掌握基礎知識嗎？所以明智的做法是從#b基礎訓練#k開始。哦，我知道你忘記所有事了。那麼這就是為什麼我出現在這裡。 你必需要自己體驗一下。那我們開始吧？");
     } else if (status == 3) {
 	qm.forceStartQuest();
 	qm.AranTutInstructionalBubble("Effect/OnUserEff.img/guideEffect/aranTutorial/tutorialArrow3");
