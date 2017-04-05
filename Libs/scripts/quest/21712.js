e9664498ba2eedcb9a4b9fedf538b4acc2adaa40
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-06 01:13:26 +0800

    npc:布魯斯 map:104000000(弓箭手村) quest:完成首次情報收集
    翻譯

--- Libs/scripts/quest/21712.js
+++ Libs/scripts/quest/21712.js
@@ -5,21 +5,21 @@ function start(mode, type, selection) {
 	status++;
     } else {
 	if (status == 2) {
-	    qm.sendNext("You still don't understand what's going on? I'll explain it to you again if you talk to me one more time.");
+	    qm.sendNext("你不明白發生什麼事了嗎?和我再次對話我會跟你解釋。");
 	    qm.dispose();
 	    return;
 	}
 	status--;
     }
     if (status == 0) {
-	qm.sendNext("#t4032315#... #rThis puppet is making a strange noise#k. You can't hear it with your ears, of course, since it can only be heard by the #o1210102#s. I believe it's this noise that changed the personality of the #o1210102#s.");
+	qm.sendNext("#t4032315#... #r這個木偶會發出奇怪的噪音#k。 你的耳多聽不到? 當然，只有#o1210102#能夠聽得到。 我像是這是#o1210102#發出的變異的聲音。");
     } else if (status == 1) {
-	qm.askAcceptDecline("The #o1210102#s that have been affected by the noise have turned cynical. They've started fighting the non-affected #o1210102#s, which has made all #o1210102#s prepare for combat. #bThe reason for all these changes in the #o1210102#s is this puppet#k! Do you understand?");
+	qm.askAcceptDecline("這些#o1210102#受到噪音的影響已經變得憤世嫉俗。他們已經開始打擊不受影響的#o1210102#，這使得所有的#o1210102#已經開始準備戰鬥。#b#o1210102#的變化是因為這個木偶#k!你明白了嗎?");
     } else if (status == 2) {
 	qm.forceStartQuest();
-	qm.sendNextS("I wonder what triggered this in the first place. There is no way this puppet was naturally created, which means someone planned this. I should keep an eye on the #o1210102#s.", 9);
+	qm.sendNextS("我不知道是什麼觸發了這一點。這個傀儡是沒有辦法自然創造的，這意味著有人計劃這個。我應該繼續注意#o1210102#。", 9);
     } else if (status == 3) {
-	qm.sendPrevS("#b(You were able to find out what caused the changes in the #o1210102#s. You should report to #p1002104# and deliver the information you''ve gathered.)#k", 2);
+	qm.sendPrevS("#b(你弄清楚了導致#o1210102#的變化，你應該向#p1002104#報告並且提供您收集的信息。)#k", 2);
 	qm.dispose();
     }
 }
