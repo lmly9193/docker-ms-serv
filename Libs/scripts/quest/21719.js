9e336e0d40db5319e5bfb6967ca0ab8092045a93
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-06 01:42:06 +0800

    npc:魁儡師 map:頭上的燈泡 quest:魁儡的邀請

--- Libs/scripts/quest/21719.js
+++ Libs/scripts/quest/21719.js
@@ -11,11 +11,11 @@ function start(mode, type, selection) {
 	status--;
     }
     if (status == 0) {
-	qm.sendNextS("Aren''t you the one that used to be in #m101000000# until not too long ago? I finally found you! Do you know how long it took for me to finally find you?", 8);
+	qm.sendNextS("你不是我在#m101000000#所看的人嗎?我終於找到你了！ 你可知道我找你找了好久了？", 8);
     } else if (status == 1) {
-	qm.sendNextPrevS("Who are you?", 2);
+	qm.sendNextPrevS("你是誰?", 2);
     } else if (status == 2) {
-	qm.askAcceptDecline("Me? If you want to know, stop by my cave. I'll even send you an invitation. You'll be directly sent to my cave as soon as you accept. Look forward to seeing you there.");
+	qm.askAcceptDecline("我? 如果你想知道，來我的洞穴吧! 我邀請你... 一旦你接受，你就會直接被傳送到我的洞穴。我們在那相見!");
     } else if (status == 3) {
 	qm.forceCompleteQuest();
 	qm.warp(910510200, 0);
