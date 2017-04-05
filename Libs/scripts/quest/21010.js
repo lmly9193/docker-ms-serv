be8f4152e6b4d3f28902478d08a2dfce58f1ceec
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-05 20:47:18 +0800

    NPC:噗卡(企鵝)Q:英雄的歸來 修改內容"翻譯"

--- Libs/scripts/quest/21010.js
+++ Libs/scripts/quest/21010.js
@@ -10,26 +10,26 @@ function start(mode, type, selection) {
 	status++;
     } else {
 	if (status == 3) {
-	    qm.sendNext("No no no, you don't have to say no. It's just a potion, anyway. Besides, for a hero like you, I can give you these all day! Let me know when you change your mind.");
+	    qm.sendNext("不不不，你不能說不要。不管怎麼說，這只是個藥水。此外，像你這樣的英雄，我可以隨時幫助你。但如果你改變主意，請讓我知道!");
 	    qm.dispose();
 	    return;
 	}
 	status--;
     }
     if (status == 0) {
-	qm.sendNext("Hmmm? What's human doing here? Wait, hey there #p1201000#. What brought you here? Oh and... do you know this person, #p1201000#? What? A hero?");
+	qm.sendNext("嗯...?你是什麼人怎麼在這?等等...嘿!是莉琳!!這個人是誰? 哦!莉琳你認識他?什麼?一個英雄?...");
     } else if (status == 1) {
-	qm.sendNextPrev("     #i4001170#");
+	qm.sendNextPrev("#i4001170#");
     } else if (status == 2) {
-	qm.sendNextPrev("Wait, so I am looking at the very person that your race has been waiting for hundreds of years? Wow!! I could tell the hero looked a bit different from the rest...");
+	qm.sendNextPrev("等等，所以我現正看著妳一直以來在等待的那個妳口中的英雄?哇!不過...他看起來有點虛弱...");
     } else if (status == 3) {
-	qm.askAcceptDecline("But because of that curse of the Black Wizard that got you trapped in ice for hundreds of years, you do look quite weak. #bHere's a potion for recovery. Please take it#k.");
+	qm.askAcceptDecline("可能是#r黑巫師#k詛咒的關係。 #b來!這是一種恢復藥水，請拿著#k。");
     } else if (status == 4) { // TODO HP set to half
-	qm.sendNext("Just drink it up first, then we'll continue our talk!");
+	qm.sendNext("然後，把它喝了，我們再繼續談吧!");
 	qm.gainItem(2000022, 1);
 	qm.forceStartQuest();
     } else if (status == 5) {
-	qm.sendNextPrevS("#b(Wait, how do I drink this? I don't remember...)#k", 3);
+	qm.sendNextPrevS("#b(等等，這要怎麼喝?我忘了......)#k", 3);
     } else if (status == 6) {
 	qm.summonMsg(0xE);
 	qm.dispose();
@@ -43,11 +43,11 @@ function end(mode, type, selection) {
 	status--;
     }
     if (status == 0) {
-	qm.sendNext("I've been searching through blocks of ice inside the cave in hopes of finding our hero, but... I didn't think I'd actually see one in front of me right now! The prophecy is correct! #p1201000#, you were right! Now that the hero has been resurrected, we won't have to worry about the Black Wizard anymore, right?");
+	qm.sendNext("我一直在洞穴裡收尋任何的冰塊，希望能找到我們的英雄，但是...我並不認為他會是真的存在!看來預言是真的!#b莉琳#k，妳是對的!現在英雄回來了，我們應該不用再擔心#r黑巫師#k了吧?");
     } else if (status == 1) {
-	qm.sendNextPrev("Wait, I have been holding onto you for too long. I'm sorry, but I bet you other penguins will react the same way as I did. I know you're busy and all, but on your way to town, #bplease go strike up a conversation with other penguins#k. Everyone will be shocked if the hero is the one initiating a conversation with them! \n\r #fUI/UIWindow.img/QuestIcon/4/0# \r #i2000022# #t2000022# 5 \r #i2000023# #t2000023# 5 \n\r #fUI/UIWindow.img/QuestIcon/8/0# 16 exp");
+	qm.sendNextPrev("等等，抱歉，我一直抱著你，哈哈!但我敢打賭，其他的企鵝也會有像我一樣的反應。#b請去與其他企鵝打交道#k。如果你和他們對話的話，他們一定很震驚，哈哈...\n\r #fUI/UIWindow.img/QuestIcon/4/0# \r #i2000022# #t2000022# 5 \r #i2000023# #t2000023# 5 \n\r #fUI/UIWindow.img/QuestIcon/8/0# 16 exp");
     } else if (status == 2) {
-	qm.sendNextPrev("Wow, you managed to level up! That means you may have acquired skill points too. In the world of Maple, every level up means 3 skill points. Press #bK#k to open the skill window and find out.");
+	qm.sendNextPrev("哇，你升級了!在這個世界只要升等，就會獲得3點技能點。請按下 #bK#k 開啟技能欄。");
 	if (qm.getQuestStatus(21010) == 1) {
 	    qm.gainExp(16);
 	    qm.gainItem(2000022, 5);
@@ -55,7 +55,7 @@ function end(mode, type, selection) {
 	    qm.forceCompleteQuest();
 	}
     } else if (status == 3) {
-	qm.sendNextPrevS("#b(These penguins are so nice to me in every way possible, yet I don't remember them one bit. I better check the skill window first... but how do I do that?)#k");
+	qm.sendNextPrevS("#b(這些企鵝看起來很和善，但我根本不記得他們了...)#k");
     } else if (status == 4) {
 	qm.summonMsg(0xF);
 	qm.dispose();
