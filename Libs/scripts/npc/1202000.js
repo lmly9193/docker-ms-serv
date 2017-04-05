1287125172fcf760475362756c91d0e45ad40817
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-05 20:09:54 +0800

    npc:莉琳 狂狼勇士甦醒 修改內容:"翻譯"

--- Libs/scripts/npc/1202000.js
+++ Libs/scripts/npc/1202000.js
@@ -16,7 +16,7 @@ function action(mode, type, selection) {
     }
 if (cm.getPlayer().getMapId() != 140090000) {
 if (status == 0) {
-	cm.sendSimple("Wait! The information listed below can all be obtained simply by playing through Level 10, so it's not something you'll need to learn way in advance. Only the ones that would like to learn these in advance should continue from here on out. \n\r Okay, which of these would you like to learn more of?  \n\r #b#L1#Minimap#l \n\r #b#L2#Quest window#l \n\r #b#L3#Inventory#l \n\r #b#L4#Regular attacks#l \n\r #b#L5#Picking up items#l \n\r #b#L6#Equipping an item#l \n\r #b#L7#Skill window#l \n\r #b#L8#How to use Quick Slot#l \n\r #b#L9#Breaking the box#l \n\r #b#L10#Sitting on a chair#l \n\r #b#L11#Raising stats#l");
+	cm.sendSimple("等等! 下面列出的信息等你10級後可以獲得，所以不需要提前了解。 只有想要提前了解才要從這裡開始 \n\r 好的，哪一個是你想先了解的?  \n\r #b#L1#Minimap#l \n\r #b#L2#任務欄#l \n\r #b#L3#物品欄#l \n\r #b#L4#攻擊#l \n\r #b#L5#Picking up items#l \n\r #b#L6#裝備欄#l \n\r #b#L7#技能欄#l \n\r #b#L8#如何使用技能快捷鍵#l \n\r #b#L9#打箱子#l \n\r #b#L10#坐椅子#l \n\r #b#L11#提高能力值#l");
 } else {
     cm.summonMsg(selection);
     cm.dispose();
@@ -24,15 +24,15 @@ if (status == 0) {
 } else {
     if (cm.getInfoQuest(21019).equals("")) {
 	if (status == 0) {
-	    cm.sendNext("You... are finally awake!");
+	    cm.sendNext("你終於醒了！");
 	} else if (status == 1) {
-	    cm.sendNextPrevS("...who are you?", 2);
+	    cm.sendNextPrevS("...妳是誰?", 2);
 	} else if (status == 2) {
-	    cm.sendNextPrev("I've been waiting for you. Waiting for the hero that fought the Black Wizard to finally wake up...!");
+	    cm.sendNextPrev("我一直在等你醒來。等待著曾與#r黑巫師#k戰鬥英雄醒來!就是你...");
 	} else if (status == 3) {
-	    cm.sendNextPrevS("Wait, what are you saying? And who are you...?", 2);
+	    cm.sendNextPrevS("等等，妳再說什麼??", 2);
 	} else if (status == 4) {
-	    cm.sendNextPrevS("Wait... who am I...? I don't remember a thing from the past. Ouch... and I have a terrible headache!", 2);
+	    cm.sendNextPrevS("兒...等等...我是誰?我不記得任何事了。我...我頭好痛!!", 2);
 	} else if (status == 5) {
 	    cm.updateInfoQuest(21019, "helper=clear");
 	    cm.showWZEffect("Effect/Direction1.img/aranTutorial/face");
@@ -42,19 +42,19 @@ if (status == 0) {
 	}
     } else {
 	if (status == 0) {
-	    cm.sendNext("Are you okay?");
+	    cm.sendNext("你還好嗎?");
 	} else if (status == 1) {
-	    cm.sendNextPrevS("I... really don't remember a thing... Where am I? And who are you?", 2);
+	    cm.sendNextPrevS("我...不記得任何事了...我在哪裡?你是誰?", 2);
 	} else if (status == 2) {
-	    cm.sendNextPrev("Relax. The curse of the Black Wizard is the reason why you don't have any memories with you. There's no need to worry about what happened in the past. I'll explain it to you in detail.");
+	    cm.sendNextPrev("放輕鬆。可能是#b黑巫師#k的詛咒讓你沒有任何記憶。別擔心，我會好好跟你解釋的。");
 	} else if (status == 3) {
-	    cm.sendNextPrev("You're a bonafide hero here. A few hundred years ago, you and your friends battled the Black Wizard and saved the world of Maple from a certain destruction. But at the last possible moment, the Black Wizard gave you a curse, which had you frozen in ice for a long time while completely wiping out your memory.");
+	    cm.sendNextPrev("幾百年前，你曾是這裡的英雄。你和你的夥伴為了拯救世界曾與#r黑巫師#k在這裡戰鬥過，當你們封印它時，可能給你下了詛咒使你凍結，也有可能這原因使你沒記憶...");
 	} else if (status == 4) {
-	    cm.sendNextPrev("You are currently at an island called Rien, and it's the island the Black Wizard chose to trap you for hundreds of years. Because of his curse, this island is always covered in snow and ice, even though the weather is nothing close to that level. You were found somewhere deep in the cave.");
+	    cm.sendNextPrev("你目前在一個叫做#b瑞恩#k的島上，這裡已經被#r黑巫師#k詛咒了。由於被詛咒的關係，這座島一直以來下著雪總是被雪蓋住。而我們就發現你在洞穴的深處。");
 	} else if (status == 5) {
-	    cm.sendNextPrev("And my name is Lirin, a member of the Rien race. The Rien race has been holding hope for your return for centuries, and now... the hope has finally paid off. You are here, standing right in front of me, the living breathing legend.");
+	    cm.sendNextPrev("我的名子叫#b莉琳#k，是瑞恩種族。我們一直盼望著你的歸來，現在終於有希望了!你就站在這，站在我們的面前...");
 	} else if (status == 6) {
-	    cm.sendNextPrev("I may have given you too much information all at once. If you have yet to catch on to everything, then that's okay. You'll find out, sooner or later. In the mean time, #byou should head over to town#k. If you have any questions before getting to town, please feel free to ask me.");
+	    cm.sendNextPrev("我可能一次給你太多訊息了...抱歉...因為我太興奮了，哈哈。如果你還搞不懂，沒關係。等等到了城鎮，你還是可以找到我。");
 	} else if (status == 7) {
 	    cm.playerSummonHint(true);
 	    cm.warp(140090100, 1);
