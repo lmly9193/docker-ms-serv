1908784da761f3cf40fbb369916f6a41cbc89772
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-05 22:50:26 +0800

    npc:莉琳 map:1400000000 quest:最後的訓練
    翻譯

--- Libs/scripts/quest/21018.js
+++ Libs/scripts/quest/21018.js
@@ -5,16 +5,16 @@ function start(mode, type, selection) {
 	status++;
     } else {
 	if (status == 1) {
-	    qm.sendNext("Oh, is 5 not enough? If you feel the need to train further, please feel free to slay more than that. If you slay all of them, I''ll just have to look the other way even if it breaks my heart, since they will have been sacrificed for a good cause...");
+	    qm.sendNext("哦，還不夠嗎？ 如果你覺得需要進一步訓練，請隨意殺死更多的東西。 如果你殺了所有的人，即使傷了我的心，我也只好看著對方，因為他們為一個很好的事被犧牲...");
 	    qm.dispose();
 	    return;
 	}
 	status--;
     }
     if (status == 0) {
-	qm.sendNext("Now, you will undergo a test that will determine whether you''re fit or not. All you have to do is take on the most powerful monster on this island, #o0100134#s. About #r50#k of them would suffice, but...");
+	qm.sendNext("現在，你將進行最後測試，以確定你是否適合。 你需要做的是消滅在這個島上最強大的怪物#o0100134#. 大約#r50#k隻，但...");
     } else if (status == 1) {
-	qm.askAcceptDecline("We can't have you wipe out the entire population of #o0100134#s, since they aren't many of them out there. How about 5 of them? You're here to train, not to destroy the ecosystem.");
+	qm.askAcceptDecline("我們不能讓你完全消滅#o0100134#，他們數目不多。5隻如何呢? 你在這裡訓練，不要破壞生態阿。");
     } else if (status == 2) {
 	qm.forceStartQuest();
 	qm.AranTutInstructionalBubble("Effect/OnUserEff.img/guideEffect/aranTutorial/tutorialArrow1");
