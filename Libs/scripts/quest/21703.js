131215b9ac0be087ef057136b81d6e29f2b70084
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-06 00:18:57 +0800

    npc:普歐(企鵝) map:140010100 quest:只有修練，才能生存下去3
    翻譯

--- Libs/scripts/quest/21703.js
+++ Libs/scripts/quest/21703.js
@@ -5,29 +5,29 @@ function start(mode, type, selection) {
 	status++;
     } else {
 	if (status == 6) {
-	    qm.sendNext("I know it takes an incredible amount of strength and will to outdo your instructor, but you weren't meant to let yourself wither away. You must move on to bigger and better things! You must do everything you can to embrace your heroic nature!");
+	    qm.sendNext("我知道一個難以置信的力量，並且會超過你的導師，但你不是要讓自己消失。 你必須走上更好的事情！ 你必須竭盡全力擁抱你的英雄能力！");
 	    qm.dispose();
 	    return;
 	}
 	status--;
     }
     if (status == 0) {
-	qm.sendNext("Your abilities are really beginning to take shape. I am surprised that an old man like me was able to help you. I''m tearing up just thinking about how happy it makes me to have been of assistance to you. *Sniff sniff*");
+	qm.sendNext("你的能力真的開始成長了。我很驚訝，像我這樣的老人能夠幫助你。 我只是在想著能夠幫助你是多麼好的一件事。 *唉~*");
     } else if (status == 1) {
-	qm.sendNextPrevS("#b(You didn't even train that long with him... Why is he crying?)#k", 2);
+	qm.sendNextPrevS("#b(你和他的訓練沒很久......為什麼他在哭?)#k", 2);
     } else if (status == 2) {
-	qm.sendNextPrev("Alright, here''s the third and the final stage of training. Your last opponent is... #r#o9300343#s#k! Do you know anything about #o1210100#s?");
+	qm.sendNextPrev("好的，這是訓練的第三個階段也是最後一個階段。你最後的對手是... #r#o9300343##k!你知道關於#o1210100#的事嗎?");
     } else if (status == 3) {
-	qm.sendNextPrevS('Well, a little bit...', 2);
+	qm.sendNextPrevS('好像...只有一點點...', 2);
     } else if (status == 4) {
-	qm.sendNextPrev("They are natural warriors! They''re born with a voracious appetite for food. They devour any food that''s visible the moment they sweep by. Terrifying, isn''t it?");
+	qm.sendNextPrev("他們是天生的戰士！ 他們出生時對食物的渴望很大。 他們吞噬任何東西，只要一瞬間任何東西都會被吞食。很可怕，不是嗎");
     } else if (status == 5) {
-	qm.sendNextPrevS("#b(Is that really true?)#k", 2);
+	qm.sendNextPrevS("#b(這是真的嗎?)#k", 2);
     } else if (status == 6) {
-	qm.askAcceptDecline("Okay, now... #bEnter the Training Center again#k, defeat #r30#k #o9300343#s, and show me what you're made of! You'll have to exert all your energy to defeat them! Go, go, go! Rise above me!");
+	qm.askAcceptDecline("好吧，現在 #b再進去一次訓練場#k, 打敗#r30#k隻#o9300343#，展你你的能力！你必須竭盡全力打敗他們！ 衝啊！ 超越我吧！");
     } else if (status == 7) {
 	qm.forceStartQuest();
-	qm.sendOk("Now go and take on those monstrous #o9300343#s!");
+	qm.sendOk("現在去解決#o9300343#吧!");
 	qm.dispose();
     }
 }
@@ -37,28 +37,28 @@ function end(mode, type, selection) {
 	status++;
     } else {
 	if (status == 2) {
-	    qm.sendNext("Are you reluctant to leave your instructor? *Sniff sniff* I'm so moved, but you can't stop here. You are destined for bigger and better things!");
+	    qm.sendNext("你是不想離開導師嗎? *啜泣* 我好感動，但你不能一直待在這。 你有更重要的事要做!");
 	    qm.dispose();
 	    return;
 	}
 	status--;
     }
     if (status == 0) {
-	qm.sendNext("Ah, you've come back after defeating all 30 #o9300343#s. I knew you had it in you... Even though you have no memories and few abilities, I could see that you were different! How? Because you're carrying around a Polearm, obviously!");
+	qm.sendNext("啊，你打敗了30隻#o9300343#，且回來了... 我可以從你身上知道...即使你忘了任何東西，還是能發會出力量!為什麼?因為你是使用矛的勇士，而你正在使用他...!");
     } else if (status == 1) {
-	qm.sendNextPrevS("#b(Is he pulling your leg?)#k'", 2);
+	qm.sendNextPrevS("#b(他是在拉著你的腿嗎?)#k'", 2);
     } else if (status == 2) {
-	qm.sendYesNo("I have nothing more to teach you, as you've surpassed my level of skill. Go now! Don't look back! This old man is happy to have served as your instructor.");
+	qm.sendYesNo("我沒有什麼可以教你的，因為你已經超過了我的能力了。 現在出發！ 不要回頭！ 我這位老人很高興成為你的導師。");
     } else if (status == 3) {
 	if (qm.getQuestStatus(21703) == 1) {
 	    qm.forceCompleteQuest();
 	    qm.teachSkill(21000000, qm.getPlayer().getSkillLevel(21000000), 10);   // Combo Ability Skill
 	    qm.gainExp(2800);
 	}
-	qm.sendNextS("(You remembered the #bCombo Ability#k skill! You were skeptical of the training at first, since the old man suffers from Alzheimer's and all, but boy, was it effective!)", 2);
+	qm.sendNextS("(你想起了#b矛之鬥氣#k!你開始相信這位導師的訓練了...)", 2);
 	qm.AranTutInstructionalBubble("Effect/BasicEff.img/AranGetSkill");
     } else if (status == 4) {
-	qm.sendPrev("Now report back to #p1201000#. I know she'll be ecstatic when she sees the progress you've made!");
+	qm.sendPrev("現在回去回報#p1201000#，我相信他知道你進步了，一定會為你感到驕傲。");
 	qm.dispose();
     }
 }
