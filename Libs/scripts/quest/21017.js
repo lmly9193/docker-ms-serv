7eb7a9f8f896d1e91aced5a0d08147fb6b5bb731
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-05 22:39:33 +0800

    npc:莉琳 map:1400000000 quest:基礎訓練3
    翻譯

--- Libs/scripts/quest/21017.js
+++ Libs/scripts/quest/21017.js
@@ -5,29 +5,29 @@ function start(mode, type, selection) {
 	status++;
     } else {
 	if (status == 5) {
-	    qm.sendNextS("#b(You declined out of fear, but it''s not like you can run away like this. Take a big breath, calm down, and try again.)#k", 2);
+	    qm.sendNextS("#b(你要拒絕恐懼，但不是逃跑。先吸一口氣，冷靜下來，再試一次...)#k", 2);
 	    qm.dispose();
 	    return;
 	}
 	status--;
     }
     if (status == 0) {
-	qm.sendNextS("It seems like you''re warmed up now. This is when rigorous training can really help you build a strong foundation. Let''s proceed with the Basic Training, shall we?", 8);
+	qm.sendNextS("看來你現在已經熱身完了。 這是嚴格的訓練是可以幫助你建立堅實的基礎。 我們繼續進行，走吧？", 8);
     } else if (status == 1) {
-	qm.sendNextPrevS("Go defeat some #r#o0100133#s#k in #b#m140020200##k this time. I think about  #r20#k should do it. Go on ahead and... Hm? Do you have something you''d like to say?", 8);
+	qm.sendNextPrevS("是時候去擊敗一些#r#o0100133##k在#b#m140020200##k。我相信#r20#k之應該難不倒你。去吧然後... 嗯?  你有什麼想說的嗎?", 8);
     } else if (status == 2) {
-	qm.sendNextPrevS("Isn''t the number getting bigger and bigger?", 2);
+	qm.sendNextPrevS("怎麼越打越多隻?", 2);
     } else if (status == 3) {
-	qm.sendNextPrevS("Of course it is. What, are you not happy with 20? Would you like to defeat 100 of them instead? Oh, how about 999 of them? Someone in Sleepywood would be able to do it easily. After all, we are training...", 8);
+	qm.sendNextPrevS("當然!!不然呢?這是訓練，要訓練當然要一次比一次難，一次比一次多啊...", 8);
     } else if (status == 4) {
-	qm.sendNextPrevS("Oh no, no, no. Twenty is plenty", 2);
+	qm.sendNextPrevS("不~好多...", 2);
     } else if (status == 5) {
-	qm.askAcceptDecline("You don't have to be so modest. I understand your desire to quickly become the hero you once were. This sort of attitude is what makes you a hero.");
+	qm.askAcceptDecline("別這樣!你想想，什麼樣的態度能讓你成為一個英雄?");
     } else if (status == 6) {
 	qm.forceStartQuest();
-	qm.sendNextS('#b(You accepted, thinking you might end up having to 999 of them if you let her keep talking.)#k', 2);
+	qm.sendNextS('#b(你接受了，別再多說了不然他又要碎碎唸...)#k', 2);
     } else if (status == 7) {
-	qm.sendNextPrevS('Please go ahead and slay 20 #o0100133#s.', 8);
+	qm.sendNextPrevS('請繼續，殺死20隻#o0100133#完成任務。', 8);
     } else if (status == 8) {
 	qm.AranTutInstructionalBubble("Effect/OnUserEff.img/guideEffect/aranTutorial/tutorialArrow3");
 	qm.dispose();
