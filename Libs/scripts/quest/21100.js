50a5a05c43db13a47e091b47d6f044a9e41520c5
Michaelyu0120 <michaelyu0120@gmail.com>
2017-04-05 23:11:38 +0800

    npc:莉琳 map:1400000000 quest:[轉職]五位英雄
    翻譯

--- Libs/scripts/quest/21100.js
+++ Libs/scripts/quest/21100.js
@@ -5,31 +5,31 @@ function start(mode, type, selection) {
 	status++;
     } else {
 	if (status == 6) {
-	    qm.sendNext("Oh, is 5 not enough? If you feel the need to train further, please feel free to slay more than that. If you slay all of them, I''ll just have to look the other way even if it breaks my heart, since they will have been sacrificed for a good cause...");
+	    qm.sendNext("哦，還不夠嗎？ 如果你覺得需要進一步訓練，請隨意殺死更多的東西。 如果你殺了所有的人，即使傷了我的心，我也只好看著對方，因為他們為一個很好的事被犧牲..."");
 	    qm.dispose();
 	    return;
 	}
 	status--;
     }
     if (status == 0) {
-	qm.sendNextS("There isn't much record left of the heores that fought against the Black Mage. Even in the Book of Prophecy, the only information available is that there were five of them. There is nothing about who they were or what they looked like. Is there anything you remember? Anything at all?", 8);
+	qm.sendNextS("與#r黑魔法師#k戰鬥的記載訊息不多。即使在#b預言書#k中，只記載有5為英雄，而他們的是誰和背景並沒有詳細說明。你有記得什麼嗎?", 8);
     } else if (status == 1) {
-	qm.sendNextPrevS("I don't remember a thing...", 2);
+	qm.sendNextPrevS("我不記得了...", 2);
     } else if (status == 2) {
-	qm.sendNextPrevS("As I expected. Of course, the curse of the Black Mage was strong enough to wipe out all of your memory. But even if that''s the case, there has got to be a point where the past will uncover, especially now that we are certain you are one of the heroes. I know you've lost your armor and weapon during the battle but... Oh, yes, yes. I almost forgot! Your #bweapon#k!", 8);
+	qm.sendNextPrevS("也是...不過我們相信你是5位英雄其中之一。然而，我們知道你在戰鬥中失去了裝備和武器，但是...哦#b(莉琳好像想到什麼)#k，是的，是的。我差點忘了!!你的#b武器#k...", 8);
     } else if (status == 3) {
-	qm.sendNextPrevS("My weapon?", 2);
+	qm.sendNextPrevS("我的武器?", 2);
     } else if (status == 4) {
-	qm.sendNextPrevS("I found an incredible weapon while digging through blocks of ice a while back. I figured the weapon belonged to a hero, so I brought it to town and placed it somewhere in the center of the town. Haven't you seen it? #bThe #p1201001##k... \r\r#i4032372#\r\rIt looks like this...", 8);
+	qm.sendNextPrevS("我們在附近的冰塊裡，發現一個不可思議的武器。 我認為這個武器屬於5位其中一位英雄的，所以我把它帶到了鎮上，把它放在了鎮中心的某個地方。 你沒看到嗎?一個#b#p1201001##k... \r\r#i4032372#\r\r它就像這樣...", 8);
     } else if (status == 5) {
-	qm.sendNextPrevS("Come to think of it, I did see a #p1201001# in town.", 2);
+	qm.sendNextPrevS("我想一下，有我有看到一個#p1201001#在城市裡。", 2);
     } else if (status == 6) {
-	qm.askAcceptDecline("Yes, that's it. According to what's been recorded, the weapon of a hero will recognize its rightful owner, and if you''re the hero that used the #p1201001#, the #p1201001# will react when you grab the #p1201001#. Please go find the #b#p1201001# and click on it.#k");
+	qm.askAcceptDecline("對!!就是它。根據記載，英雄的武器會認主人的，如果你是#p1201001#的主人,當你拿起#p1201001#，#p1201001#應該會有反應。去拿看看#b#p1201001##k，搞不好會有什麼事發生...");
     } else if (status == 7) {
 	if (qm.getQuestStatus(21100) == 0) {
 	    qm.forceCompleteQuest();
 	}
-	qm.sendOkS("If the #p1201001# reacts to you, then we'll know that you're #bAran#k, the hero that wielded a #p1201001#.", 8);
+	qm.sendOkS("如果#p1201001#對你有反應，那我們就能確認你是傳說中#b亞蘭(狂狼勇士)#k，那位英雄就是揮舞著#p1201001#。", 8);
 	qm.showWZEffect("Effect/Direction1.img/aranTutorial/ClickPoleArm");
 	qm.dispose();
     }
